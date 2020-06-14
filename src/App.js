38735ec779d2626f7da6928d9cf1addebbc690a4
Anant Prasad <anant29october@gmail.com>
2020-06-14 12:02:14 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseForm from "./components/ExpenseForm";
+import Alert from "./components/Alert";
+import uuid from "uuid/v4";
+const initialExpenses = [
+  { id: uuid(), charge: "rent", amount: 1600 },
+  { id: uuid(), charge: "car payment", amount: 400 },
+  { id: uuid(), charge: "credit card bill", amount: 1200 }
+];
+console.log(initialExpenses);
+
+function App() {
+  return (
+    <>
+      <Alert />
+      <ExpenseForm />
+      <ExpenseList />
+    </>
+  );
+}
+
+export default App;
