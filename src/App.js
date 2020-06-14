4600d6143c7ed5400df458acb4a093078e4118f9
Anant Prasad <anant29october@gmail.com>
2020-06-14 12:18:35 +0530

    total spendings

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{ useState } from "react";
 import "./App.css";
 import ExpenseList from "./components/ExpenseList";
 import ExpenseForm from "./components/ExpenseForm";
@@ -9,14 +9,30 @@ const initialExpenses = [
   { id: uuid(), charge: "car payment", amount: 400 },
   { id: uuid(), charge: "credit card bill", amount: 1200 }
 ];
-console.log(initialExpenses);
+// console.log(initialExpenses);
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
+
+
+
   return (
     <>
       <Alert />
+      <h1>budget calculator</h1>
+      <main className="App">
       <ExpenseForm />
-      <ExpenseList />
+      <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        total spending : <span className="total">
+          $ {expenses.reduce((acc,curr)=>{
+            return (acc+= curr.amount);
+          },0)}
+        </span>
+      </h1>
+      
     </>
   );
 }
